3f2208edad2b087fc60df3ab90ddfa1cafd1ae13
abhishek199677 <108387488+abhishek199677@users.noreply.github.com>
2024-11-25 11:52:37 +0530

    requirments added

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Final-End-to-end-ML-Project
+
+
+
+<!-- STEPS:
+
+conda create -n mlproj python=3.8 -y 
+conda activate mlproj
+pip install -r requirements.txt
+python app.py -->
---
+++ setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+
+__version__ = "0.0.0"
+
+REPO_NAME = "End-to-end-ML-Project-Implementation"
+AUTHOR_USER_NAME = "abhishek199677"
+SRC_REPO = "mlProject"
+AUTHOR_EMAIL = "puttapoguabhishek@gmail.com"
+
+
+ 
